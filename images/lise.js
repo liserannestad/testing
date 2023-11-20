2cc60734ade7e2cebf9af838f3549c98d1d82dad
liserannestad <144259618+liserannestad@users.noreply.github.com>
2023-11-20 18:35:42 +0100

    Update lise.js

--- images/lise.js
+++ images/lise.js
@@ -4,11 +4,11 @@ import { rectangle, circle, triangle, circlesvg, rectanglesvg, trianglesvg } fro
 function canvaslise() {
 	circle("black", 75, 75, 61);
 	circle("pink", 75, 75, 60);
-	circle("white", 30, 34, 10);
-	circle("white", 70, 34, 10);
+	circle("white", 60, 50, 10);
+	circle("white", 90, 50, 10);
 	rectangle("black", 71,5, 60, 14, 4);
-	circle("black", 33, 37, 7);
-	circle("black", 73, 37, 7);
+	circle("black", 60, 50, 7);
+	circle("black", 90, 50, 7);
 }
 
 canvaslise();
