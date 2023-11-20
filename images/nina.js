1e9ceb34f15137a9357d606bce301f6da45325bb
liserannestad <144259618+liserannestad@users.noreply.github.com>
2023-11-20 18:31:39 +0100

    endrign

--- images/nina.js
+++ images/nina.js
@@ -1,20 +1,20 @@
 import { rectangle, circle, triangle, circlesvg, rectanglesvg, trianglesvg } from "./figures.js";
 
 
-function canvaslise() {
+function canvasnina() {
 	circle("black", 75, 75, 61);
 	circle("pink", 75, 75, 60);
-	circle("white", 30, 34, 10);
-	circle("white", 70, 34, 10);
+	circle("white", 60, 50, 10);
+	circle("white", 90, 50, 10);
 	rectangle("black", 71,5, 60, 14, 4);
-	circle("black", 33, 37, 7);
-	circle("black", 73, 37, 7);
+	circle("black", 60, 50, 7);
+	circle("black", 90, 50, 7);
 }
 
-canvaslise();
+canvasnina();
 
 
-function svglise() {
+function svgnina() {
 	circlesvg("black", "75", "75", "61");
 	circlesvg("pink", "75", "75", "60");
 	circlesvg("white", "60", "50", "10");
@@ -24,5 +24,5 @@ function svglise() {
 	circlesvg("black", "90", "50", "7");
 }
 
-svglise();
+svgnina();
 
