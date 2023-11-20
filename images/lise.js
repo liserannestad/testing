e1b84fdc5bc9cecbf4080ec95f1229a50f479953
liserannestad <144259618+liserannestad@users.noreply.github.com>
2023-11-20 17:18:56 +0100

    endring

--- images/lise.js
+++ images/lise.js
@@ -2,8 +2,8 @@ import { rectangle, circle, triangle, circlesvg, rectanglesvg, trianglesvg } fro
 
 
 function canvaslise() {
-	circle("black", 300, 300, 61);
-	circle("pink", 300, 300, 60);
+	circle("black", 50, 50, 61);
+	circle("pink", 50, 50, 60);
 	circle("white", 30, 34, 10);
 	circle("white", 70, 34, 10);
 	rectangle("black", 53, 46, 14, 4);
@@ -15,8 +15,8 @@ canvaslise();
 
 
 function svglise() {
-	circlesvg("black", "300", "300", "61");
-	circlesvg("pink", "300", "300", "60");
+	circlesvg("black", "50", "50", "61");
+	circlesvg("pink", "50", "50", "60");
 	circlesvg("white", "30", "34", "10");
 	circlesvg("white", "70", "34", "10");
 	rectanglesvg("black", "53", "46", "14", "4");
@@ -26,10 +26,3 @@ function svglise() {
 
 svglise();
 
-overlay-xy(circle(7, "solid", "black"), -73, -37, 
-  overlay-xy(circle(7, "solid", "black"), -33, -37, 
-    overlay-xy(rectangle(14, 4, "solid", "black"), -53, -46, 
-      overlay-xy(circle(10, "solid", "white"), -70, -34, 
-        overlay-xy(circle(10, "solid", "white"), -30, -34, 
-          overlay(circle(60, "solid", "pink"),
-            circle(61, "solid", "black")))))))
