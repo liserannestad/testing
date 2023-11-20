65421877f657adcb55024d7cf8380c96827bf1c4
liserannestad <144259618+liserannestad@users.noreply.github.com>
2023-11-20 23:19:41 +0100

    dd

--- images/lise.js
+++ images/lise.js
@@ -17,11 +17,11 @@ canvaslise();
 function svglise() {
 	circlesvg("black", "75", "75", "61");
 	circlesvg("pink", "75", "75", "60");
-	circlesvg("white", "60", "50", "10");
-	circlesvg("white", "90", "50", "10");
-	rectanglesvg("black", "68", "60", "14", "4");
-	circlesvg("black", "60", "50", "7");
-	circlesvg("black", "90", "50", "7");
+	circlesvg("white", "55", "50", "10");
+	circlesvg("white", "95", "50", "10");
+	rectanglesvg("black", "68", "57", "14", "4");
+	circlesvg("black", "55", "50", "7");
+	circlesvg("black", "95", "50", "7");
 }
 
 svglise();
