e20c007976aebe108e504bd9f89f63c9ab414732
liserannestad <144259618+liserannestad@users.noreply.github.com>
2023-11-21 11:41:52 +0100

    Update lise.js

--- images/lise.js
+++ images/lise.js
@@ -6,7 +6,7 @@ function canvaslise() {
 	circle("pink", 75, 75, 60);
 	circle("white", 55, 50, 10);
 	circle("white", 95, 50, 10);
-	rectangle("black", 68, 60, 14, 4);
+	rectangle("black", 68, 55, 14, 4);
 	circle("black", 55, 50, 7);
 	circle("black", 95, 50, 7);
 }
